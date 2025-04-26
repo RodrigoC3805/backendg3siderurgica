481025a6078bf22b5a0e40b428b99aa7abcac2e5
RodrigoC3805 <rodrigocastillo3805@gmail.com>
2025-04-26 01:33:29 -0500

    Spring project created

---
+++ src/test/java/dsw/backendg3siderurgica/Backendg3siderurgicaApplicationTests.java
@@ -0,0 +1,13 @@
+package dsw.backendg3siderurgica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Backendg3siderurgicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
