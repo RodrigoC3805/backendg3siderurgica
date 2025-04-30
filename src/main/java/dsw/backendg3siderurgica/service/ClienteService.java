0373cd895c3c74bac92fcac757e360cd4637b43d
nxn24 <166672278+nxn24@users.noreply.github.com>
2025-04-30 12:43:11 -0500

    add PostMapping insertCliente (#5)

--- src/main/java/dsw/backendg3siderurgica/service/ClienteService.java
+++ src/main/java/dsw/backendg3siderurgica/service/ClienteService.java
@@ -5,18 +5,37 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import dsw.backendg3siderurgica.dto.ClienteRequest;
 import dsw.backendg3siderurgica.dto.ClienteResponse;
+import dsw.backendg3siderurgica.model.Cliente;
+import dsw.backendg3siderurgica.model.TipoCliente;
 import dsw.backendg3siderurgica.repository.ClienteRepository;
 import dsw.backendg3siderurgica.repository.TipoClienteRepository;
 
 @Service
 public class ClienteService {
     @Autowired
-    ClienteRepository empleadoRepository;
+    ClienteRepository clienteRepository;
     @Autowired
     TipoClienteRepository tipoClienteRepository;
 
     public List<ClienteResponse> listClientes() {
-        return ClienteResponse.fromEntities(empleadoRepository.findAll());
+        return ClienteResponse.fromEntities(clienteRepository.findAll());
+    }
+
+    public ClienteResponse insertCliente(ClienteRequest clienteRequest){
+        Integer idTipoCliente = clienteRequest.getIdTipoCliente();
+        TipoCliente tipoCliente = tipoClienteRepository.findById(idTipoCliente).get();
+        if(tipoCliente == null) return new ClienteResponse();
+
+        Cliente cliente = new Cliente(
+            clienteRequest.getIdCliente(),
+            clienteRequest.getNombre(),
+            clienteRequest.getRuc(),
+            tipoCliente
+        );
+
+        cliente = clienteRepository.save(cliente);
+        return ClienteResponse.fromEntity(cliente);
     }
 }
